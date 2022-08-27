1867760bdff4445a08a1b96e5e16a91abe4c2227
Osman <osman.mamedov2000@gmail.com>
2022-08-27 11:05:54 +0300

    Low quality game
    Catch th kenny win points

---
+++ CatchTheKennyKotlin/app/src/main/java/com/example/catchthekennykotlin/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.catchthekennykotlin
+
+
+import android.app.AlertDialog
+import android.content.DialogInterface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.Toast
+
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+    fun start(view: View) {
+
+    }
+    fun increaseScore(view : View) {
+
+    }
+
+
+}
